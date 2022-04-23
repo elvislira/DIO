c79590ec3d236da15dcc5cb2d1a6d700bab21551
Elvis Lira <professorelvislira@gmail.com>
2022-04-23 13:09:51 -0300

    Curso JS - Games Projects - Jogo da velha - Correções.

--- curso_javascript/games_projects/jogo-da-velha/js/jogo-da-velha.js
+++ curso_javascript/games_projects/jogo-da-velha/js/jogo-da-velha.js
@@ -1,22 +1,30 @@
 const quadrados = document.querySelectorAll('.quadrado');
 
 const jogadores = {
-    jogador1: {value: 0, name: 'O'},
-    jogador2: {value: 1, name: 'X'},
+    jogador1: {
+        value: 0, 
+        name: 'O'
+    },
+    jogador2: {
+        value: 1, 
+        name: 'X'
+    },
 }
 
 function selecionarJogador() {
-    const jogador = Math.floor(Math.random() * 2);
+    //Escolhe o id(0 ou 1) do jogador aleatoriamente
+    const idJogador = Math.floor(Math.random() * 2);
 
-    if (jogador == jogadores.jogador1.value) {
+    //Retorna o jogador cujo id foi sorteado.
+    if (idJogador == jogadores.jogador1.value) {
         return jogadores.jogador1.name;
     }
 
     return jogadores.jogador2.name;
 }
 
-function trocarJogador(jogador) {
-    if (jogador == jogadores.jogador1.name) {
+function trocarJogador(nomeJogador) {
+    if (nomeJogador == jogadores.jogador1.name) {
         return jogadores.jogador2.name;
     }
 
@@ -25,21 +33,28 @@ function trocarJogador(jogador) {
  
 let jogadorAtual = selecionarJogador();
 
-function atualizarExibicao(id) {
-    const quadrado = document.getElementById(id);
+atualizaLegendaJogador();
+
+function atualizaLegendaJogador() {
     const legendaJogador = document.getElementById('jogador-selecionado');
+
+    legendaJogador.innerHTML = jogadorAtual;
+}
+
+function atualizarExibicaoQuadrado(idQuadrado) {
+    const quadrado = document.getElementById(idQuadrado);
 
     quadrado.innerHTML = jogadorAtual;
 
     jogadorAtual = trocarJogador(jogadorAtual);
-    legendaJogador.innerHTML = jogadorAtual;
 
     quadrado.classList.add('quadrado-clicado');
 }
 
 for (let quadrado of quadrados) {
     quadrado.addEventListener('click', (event) => {
-        atualizarExibicao(quadrado.id);
+        atualizarExibicaoQuadrado(quadrado.id);
+        atualizaLegendaJogador();
     });
 }
 

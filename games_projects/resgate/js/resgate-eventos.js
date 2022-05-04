d39e70fe6884ad79754ed5eb990c67c17a70e2d5
Elvis Lira <professorelvislira@gmail.com>
2022-05-04 19:18:01 -0300

    Games - Resgate - Criação do animacao.css.

--- games_projects/resgate/js/resgate-eventos.js
+++ games_projects/resgate/js/resgate-eventos.js
@@ -1,1 +1,2 @@
-document.getElementById("inicio").addEventListener("click", start);
+$("#inicio").click(start);
+
--- games_projects/resgate/js/resgate-funcoes.js
+++ games_projects/resgate/js/resgate-funcoes.js
@@ -1,10 +1,10 @@
 function start() {
     $("#inicio").hide();
 
-    $("#fundo-game").append("<div id='jogador'></div>");
-    $("#fundo-game").append("<div id='inimigo1'></div>");
+    $("#fundo-game").append("<div id='jogador' class='anima1'></div>");
+    $("#fundo-game").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundo-game").append("<div id='inimigo2'></div>");
-    $("#fundo-game").append("<div id='amigo'></div>");
+    $("#fundo-game").append("<div id='amigo' class='anima3'></div>");
 };
 
 

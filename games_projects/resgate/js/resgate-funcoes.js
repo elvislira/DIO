c83376b33f4fb801201060312b74fbee76832d7a
Elvis Lira <professorelvislira@gmail.com>
2022-05-04 19:49:16 -0300

    Games - Resgate - Movimentação do helicóptero amigo.

--- games_projects/resgate/js/resgate-funcoes.js
+++ games_projects/resgate/js/resgate-funcoes.js
@@ -7,19 +7,52 @@ function start() {
     $("#fundo-game").append("<div id='amigo' class='anima3'></div>");
 
     var jogo = {};
+    var TECLA = {
+        W: 87, //para cima
+        S: 83, //para baixo
+        D: 68, //atirar
+    };
+
+    jogo.pressionou = [];
+
+    $(document).keydown(function(event) {
+        jogo.pressionou[event.which] = true;
+    });
+
+    $(document).keyup(function(event) {
+        jogo.pressionou[event.which] = false;
+    });
 
     jogo.timer = setInterval(loop, 20);
 
     function loop() {
         moveFundo();
+        moveJogador();
     }
 
     function moveFundo() {
         esquerda = parseInt($("#fundo-game").css("background-position"));
         $("#fundo-game").css("background-position", esquerda - 1);
     }
+
+    function moveJogador() {
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            if (topo > 0) {
+                $("#jogador").css("top", topo - 10);
+            }
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+            var topo = parseInt($("#jogador").css("top"));
+            if (topo < 434) {
+                $("#jogador").css("top", topo + 10);
+            }
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+            //Disparo
+        }
+    }
 };
-
-
-
 

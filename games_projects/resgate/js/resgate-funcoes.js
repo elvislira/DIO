09ad4418fd559ae37513889a707a0c92e49d256f
Elvis Lira <professorelvislira@gmail.com>
2022-05-04 19:28:37 -0300

    Games - Resgate - Criação do game loop.

--- games_projects/resgate/js/resgate-funcoes.js
+++ games_projects/resgate/js/resgate-funcoes.js
@@ -5,6 +5,19 @@ function start() {
     $("#fundo-game").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundo-game").append("<div id='inimigo2'></div>");
     $("#fundo-game").append("<div id='amigo' class='anima3'></div>");
+
+    var jogo = {};
+
+    jogo.timer = setInterval(loop, 20);
+
+    function loop() {
+        moveFundo();
+    }
+
+    function moveFundo() {
+        esquerda = parseInt($("#fundo-game").css("background-position"));
+        $("#fundo-game").css("background-position", esquerda - 1);
+    }
 };
 
 

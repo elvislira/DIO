9f6244a149c0673b459b815afc7b53d6572390d8
Elvis Lira <professorelvislira@gmail.com>
2022-05-06 13:38:12 -0300

    Games - Dinossauro - Implementação dos cactus.

--- games_projects/dinossauro/js/script.js
+++ games_projects/dinossauro/js/script.js
@@ -1,23 +1,31 @@
 const dino = document.querySelector('.dino');
+const fundoGame = document.querySelector('.fundo-game');
+
+let isJumping = false;
 
 document.addEventListener('keyup', pressionaTecla);
+criarCactus();
 
 function pressionaTecla(event) {
     if (event.keyCode === 32) {
-        jump();
+        if (!isJumping) {
+            jump();
+        }
     }
 }
 
 function jump() {
     let posicao = 68;
+    isJumping = true;
 
     let upInterval = setInterval(() => {
-        if (posicao >= 150) {
+        if (posicao >= 200) {
             clearInterval(upInterval);
 
             let downInterval = setInterval(() => {
                 if (posicao <= 68) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     posicao -= 20;
                     dino.style.bottom = posicao + 'px';
@@ -28,4 +36,26 @@ function jump() {
             dino.style.bottom = posicao + 'px';
         }
     }, 20);
+}
+
+function criarCactus() {
+    const cactus = document.createElement('div');
+    let posicaoCactus = 1000;
+    let tempoAleatorio = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = posicaoCactus + 'px';
+    fundoGame.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (posicaoCactus < -60) {
+            clearInterval(leftInterval);
+            fundoGame.removeChild(cactus);
+        } else {
+            posicaoCactus -= 10;
+            cactus.style.left = posicaoCactus + 'px';
+        }
+    }, 20);
+
+    setTimeout(criarCactus, tempoAleatorio);
 }

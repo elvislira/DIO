86eaced071aa9a12a9ea79862a8bb2880d2cdf1f
Elvis Lira <professorelvislira@gmail.com>
2022-05-06 19:58:43 -0300

    Games - Space Shooter - Criação dos aliens do jogo.

--- games_projects/space-shooter/js/script.js
+++ games_projects/space-shooter/js/script.js
@@ -1,5 +1,10 @@
 const playerShip = document.querySelector('.shooter');
 const playArea = document.querySelector('.game-area');
+const aliensImg = [
+    'img/monster-1.png',
+    'img/monster-2.png',
+    'img/monster-3.png',
+];
 
 window.addEventListener('keydown', flyShip)
 
@@ -69,4 +74,36 @@ function moveLaser(laser) {
             laser.style.left = `${xPosition + 8}px`;
         }
     }, 10);
+}
+
+function createAliens() {
+    let newAlien = document.createElement('img');
+    let nAlien = Math.floor(Math.random() * aliensImg.length);
+    let alienSprite = aliensImg[nAlien];
+    let alienY = Math.floor(Math.random() * 330) + 30;
+
+    newAlien.src = alienSprite;
+    newAlien.classList.add('alien');
+    newAlien.classList.add('alien-transition');
+    newAlien.style.left = '370px';
+    newAlien.style.top = `${alienY}px`;
+    playArea.appendChild(newAlien);
+
+    moveAlien();
+}
+
+function moveAlien(alien) {
+    let moveAlienInterval = setInterval(() => {
+        let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'));
+
+        if (xPosition <= 50) {
+            if (Array.from(alien.classList).includes('dead-alien')) {
+                alien.remove();
+            } else {
+                gameOver();
+            }
+        } else {
+            alien.style.left = `${xPosition - 4}px`;
+        }
+    }, 30); 
 }

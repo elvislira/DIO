343272c054c8d70f3847207d1a7bb0bece7bf4ee
Elvis Lira <professorelvislira@gmail.com>
2022-05-06 19:39:02 -0300

    Games - Space Shooter - Implementação do laser para o jogador.

--- games_projects/space-shooter/js/script.js
+++ games_projects/space-shooter/js/script.js
@@ -31,11 +31,42 @@ function moveUp() {
 function moveDown() {
     let topPosition = parseInt(getComputedStyle(playerShip).getPropertyValue('top'));
 
-    if (topPosition >= 440) {
+    if (topPosition >= 530) {
         return;
     } else {
         let position = topPosition;
         position += 50;
         playerShip.style.top = `${position}px`;
     }
+}
+
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(playerShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(playerShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition-10}px`;
+
+    return newLaser;
+}
+
+function moveLaser(laser) {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+
+        if (xPosition === 420) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`;
+        }
+    }, 10);
 }

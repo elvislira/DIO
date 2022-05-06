c0940ebede6d88703d53091677a6dfdfc8b6f2fb
Elvis Lira <professorelvislira@gmail.com>
2022-05-05 22:15:09 -0300

    Games - Resgate - Criação da barra de vidas do jogador.

--- games_projects/resgate/js/resgate-funcoes.js
+++ games_projects/resgate/js/resgate-funcoes.js
@@ -8,6 +8,7 @@ function start() {
     $("#fundo-game").append("<div id='inimigo2'></div>");
     $("#fundo-game").append("<div id='amigo' class='anima3'></div>");
     $("#fundo-game").append("<div id='placar'></div>");
+    $("#fundo-game").append("<div id='energia'></div>");
 
     var jogo = {};
     var TECLA = {
@@ -23,6 +24,7 @@ function start() {
     var pontos = 0;
     var salvos = 0;
     var perdidos = 0;
+    var energiaAtual = 3;
 
     jogo.pressionou = [];
 
@@ -44,6 +46,7 @@ function start() {
         moveAmigo();
         colisao();
         placar();
+        energia();
     }
 
     function moveFundo() {
@@ -144,6 +147,7 @@ function start() {
         let colisao6 = ($("#inimigo2").collision($("#amigo")));
         
         if (colisao1.length > 0) {
+            energiaAtual--;
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -156,6 +160,7 @@ function start() {
         }
 
         if (colisao2.length > 0) {
+            energiaAtual--;
             inimigo2X = parseInt($("#inimigo2").css("left"));
             inimigo2Y = parseInt($("#inimigo2").css("top"));
             explosao2(inimigo2X,inimigo2Y);
@@ -288,5 +293,23 @@ function start() {
     function placar() {
         $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");   
     }
+
+    function energia() {
+		if (energiaAtual == 3) {
+			$("#energia").css("background-image", "url(imgs/energia3.png)");
+		}
+	
+		if (energiaAtual == 2) {
+			$("#energia").css("background-image", "url(imgs/energia2.png)");
+		}
+	
+		if (energiaAtual == 1) {
+			$("#energia").css("background-image", "url(imgs/energia1.png)");
+		}
+	
+		if (energiaAtual == 0) {
+			$("#energia").css("background-image", "url(imgs/energia0.png)");
+		}
+	}
 };
 
